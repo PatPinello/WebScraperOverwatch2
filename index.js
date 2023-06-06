b5669d5f00436a8aa4eadcb53823aaa91e2b5ab9
MrSpankledorf <patrickpinello@gmail.com>
2023-06-05 23:48:11 -0400

    Added HTML and js to index.php to show results

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function myFunction() {
-    fetch("C:/Users/patri/Music/WebscraperOverwatch2/WebScraperOverwatch2/Ninjaman7000-1418_Eliminations_MostinGame.json")
-        .then((res) => {
-        return res.json();
-    })
-    .then((data) => alert(data));
-}
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
