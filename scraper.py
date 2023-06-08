f5774f6c95f919b5a5da6d482d9f760a2fe84e67
MrSpankledorf <patrickpinello@gmail.com>
2023-06-08 12:33:34 -0400

    Organized scraper and moved js to seperate file,

--- scraper.py
+++ scraper.py
@@ -31,33 +31,11 @@ def fillJSONDict(JSONDict, url):
     user = username.replace("'","")
     JSONDict[user] = playerDictionary
 
-
-
-
 url_ben = 'https://overwatch.blizzard.com/en-us/career/Ninjaman7000-1418/'
-# dict_ben = playerDict(url_ben)
-# username_ben_sql = "'"+url_ben.replace('https://overwatch.blizzard.com/en-us/career/','').replace('/','')+"'"
-# dict_ben['username'] = username_ben_sql
-# bestOverall_dict[username_ben_sql.replace("'","")] = dict_ben
-
 url_ty = 'https://overwatch.blizzard.com/en-us/career/DirtSoup-1478/'
-# dict_ty = playerDict(url_ty)
-# username_ty_sql = "'"+url_ty.replace('https://overwatch.blizzard.com/en-us/career/','').replace('/','')+"'"
-# dict_ty['username'] = username_ty_sql
-# bestOverall_dict[username_ty_sql.replace("'","")] = dict_ty
-
 url_jess = 'https://overwatch.blizzard.com/en-us/career/Deigs-11887/'
-# dict_jess = playerDict(url_jess)
-# username_jess_sql = "'"+url_jess.replace('https://overwatch.blizzard.com/en-us/career/','').replace('/','')+"'"
-# dict_jess['username'] = username_jess_sql
-# bestOverall_dict[username_jess_sql.replace("'","")] = dict_jess
-
-
 url_pat = 'https://overwatch.blizzard.com/en-us/career/ComradeJuice-1182/'
-# dict_pat = playerDict(url_pat)
-# username_pat_sql = "'"+url_pat.replace('https://overwatch.blizzard.com/en-us/career/','').replace('/','')+"'"
-# dict_pat['username'] = username_pat_sql
-# bestOverall_dict[username_pat_sql.replace("'","")] = dict_pat
+
 fillJSONDict(bestOverall_dict,url_ben)
 fillJSONDict(bestOverall_dict,url_ty)
 fillJSONDict(bestOverall_dict,url_jess)
