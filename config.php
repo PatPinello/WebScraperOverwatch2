40d9dffa877581f8d0ff8326aecdb7bf0b896385
Pat Pinello <patrickpinello@gmail.com>
2023-06-25 14:54:50 -0400

    fixed database name

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   require("./connectvalues.php");
 
-  $dsn        = "mysql:host=$host;dbname=tasks";
+  $dsn        = "mysql:host=$host;dbname=playerStats";
 
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
