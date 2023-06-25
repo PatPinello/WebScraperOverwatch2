6c006b18ea0d6cdab68c4425fa7711d82b627a01
Pat Pinello <patrickpinello@gmail.com>
2023-06-25 15:53:47 -0400

    update SQL Init

--- overwatch.php
+++ overwatch.php
@@ -113,7 +113,7 @@
             `OffeniveAit` float DEFAULT NULL,
             `Card` float DEFAULT NULL,
             `day` date DEFAULT NULL,
-            `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+            `username` varchar(255) DEFAULT NULL,
             `id` int NOT NULL
           );");
         
